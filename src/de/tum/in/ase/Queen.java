c4fe080bbd032b39504a90a2a670d904db34b2aa
Yunqing Wang <yunqing.wang@tum.de>
2022-10-31 14:40:05 +0100

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/Queen.java
@@ -0,0 +1,9 @@
+package de.tum.in.ase;
+
+public class Queen {
+    private static final int MAX_DISTANCE = 3;
+    //TODO finish implementing the corresponding methods and ineritance relationships
+    public static void main(String[] args) {
+        //you can do some tests here
+    }
+}
---
+++ src/de/tum/in/ase/Rook.java
@@ -0,0 +1,8 @@
+package de.tum.in.ase;
+
+public class Rook {
+    //TODO finish implementing the corresponding methods and ineritance relationships
+    public static void main(String[] args) {
+        //you can do some tests here
+    }
+}
